fef440bf52907fe0cb1df690c919a9f3caae7474
Tomasz Leś <tomasz.les@pw.edu.pl>
2025-02-06 09:43:02 +0100

    q

---
+++ Shop.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Shop.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Shop.Shared/Entity/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Shared.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Barcode { get; set; }
+
+        public double Price { get; set; } // domylsnie mapuje na decimal(18,2)
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
